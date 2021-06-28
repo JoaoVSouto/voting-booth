95d7dc59791aff08ca0542c977df8316e322ffe0
JoaoVSouto <joaovitorss99@hotmail.com>
2021-06-28 12:15:59 -0300

    :tada: Vue initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>hello world!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+});
+</script>
